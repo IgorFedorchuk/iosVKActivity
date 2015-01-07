0ce5cfb2ecbc31e9ae3623e00a39a6039622820f
Igor Fedorchuk <fedorchukjob@gmail.com>
2015-01-07 15:58:12 +0300

    Initial commit

---
+++ VKActivity/VKActivity/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VKActivity
+//
+//  Created by Igor on 1/7/15.
+//  Copyright (c) 2015 Igor Fedorchuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VKActivity/VKActivity/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VKActivity
+//
+//  Created by Igor on 1/7/15.
+//  Copyright (c) 2015 Igor Fedorchuk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
